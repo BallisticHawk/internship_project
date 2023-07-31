a119dc2ee04c53c1c620ede41d0e386615db9ab5
Sean Ng <sng3051@gmail.com>
2023-08-01 07:36:13 +0800

    First Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,33 +12,33 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Learning CyberSecurity Made Easy! 
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href=""
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Newbie</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                Just the basics - Everything you need to know to start learning CyberSecurity and Digital Forensics.
               </div>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href=""
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Veteran</h3>
               <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                Already know some knowledge about CyberSecurity or Digital Forensics? Take a quiz to strengthen your weak spots and learn more!
               </div>
             </Link>
           </div>
         </div>
+        <script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
+        <script src="https://mediafiles.botpress.cloud/74d99ce9-fd19-4c81-a073-a9490f4aa12a/webchat/config.js" defer></script>
       </main>
     </>
   );
