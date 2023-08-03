50f2bb52be6c5d5df4103722a2d015a3ab3132e1
Sean Ng <sng3051@gmail.com>
2023-08-03 10:44:06 +0800

    Navbar component update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import { type AppType } from "next/dist/shared/lib/utils";
-import "~/styles/globals.css";
+// app.tsx
+import React from 'react';
+import { AppType } from 'next/dist/shared/lib/utils';
+import '~/styles/globals.css';
+import Navbar from './navbar'; // Update the path according to your folder structure
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
