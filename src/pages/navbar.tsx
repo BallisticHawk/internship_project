16a095e65dc302cb6d31257c2b55bcc9b6de746e
Sean Ng <sng3051@gmail.com>
2023-08-14 11:35:00 +0800

    comment out certain things

--- src/pages/navbar.tsx
+++ src/pages/navbar.tsx
@@ -141,7 +141,7 @@ export default function Example() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
+                {/* <Disclosure as="div" className="-mx-3">
                   {({ open }) => (
                     <>
                       <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
@@ -150,8 +150,8 @@ export default function Example() {
                           className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
                           aria-hidden="true"
                         />
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="mt-2 space-y-2">
+                      </Disclosure.Button> */}
+                      {/* <Disclosure.Panel className="mt-2 space-y-2">
                         {[...products, ...callsToAction].map((item) => (
                           <Disclosure.Button
                             key={item.name}
@@ -164,8 +164,8 @@ export default function Example() {
                         ))}
                       </Disclosure.Panel>
                     </>
-                  )}
-                </Disclosure>
+                  )} */}
+                {/* </div></Disclosure> */}
                 {/* <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
